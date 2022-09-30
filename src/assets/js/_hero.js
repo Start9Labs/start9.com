62da575385e58b4ceb5c98f5b64a5c607e8fcbeb
Spencer Flagg <work@spencerflagg.com>
2022-09-30 18:02:17 +0200

    9/29 Lucy notes; added list.js, didn't implement

--- src/assets/js/_hero.js
+++ src/assets/js/_hero.js
@@ -1,18 +1,18 @@
 //////////////////////////////////////////
 // HERO
 
-gsap.from("#hero-image", {
-    duration: 1,
-    //y: "-10rem",
-    scale: 0.95,
-    opacity: 0,
-    filter: "blur(10px)",
-    delay: 0.5,
-  });
-  gsap.from("#logo", {
-    duration: 0.5,
-    y: "-5rem",
-    opacity: 0,
-    delay: 1.25,
-    //ease: "in-out"
-  });
+gsap.to("#hero-image", {
+  duration: 1,
+  //y: "-10rem",
+  scale: 1,
+  opacity: 1,
+  filter: "blur(0px)",
+  delay: 0.5,
+});
+gsap.to("#logo", {
+  duration: 0.5,
+  y: 0,
+  opacity: 1,
+  delay: 1.25,
+  //ease: "in-out"
+});
