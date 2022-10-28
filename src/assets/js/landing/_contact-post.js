aedac0316415b1a26996384ed8b3fbb7d343f995
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-10-28 17:35:04 -0600

    implement newsletter subscription

--- src/assets/js/landing/_contact-post.js
+++ src/assets/js/landing/_contact-post.js
@@ -2,11 +2,13 @@
 /* CONTACT POST                             */
 /********************************************/
 
-
-function showSuccess() {
-    hideError();
-    emailSuccess.classList.add("form-alert--visible");
-  }
+const emailError = document.getElementById("emailError")
+const emailSuccess = document.getElementById("emailSuccess")
+function showSuccess(message) {
+  hideError();
+  emailSuccess.classList.add("form-alert--visible");
+  emailSuccess.innerHTML = message
+}
   
   function hideSuccess() {
     emailError.classList.remove("form-alert--visible");
@@ -15,18 +17,20 @@ function showSuccess() {
   function showError(message) {
     hideSuccess();
     emailError.classList.add("form-alert--visible");
-    emailError.innerHTML(message);
+    emailError.innerHTML = message;
   }
   
   function hideError() {
     emailError.classList.remove("form-alert--visible");
   }
   
+  function clear(){
+    document.getElementById("subscribeForm").reset()
+  }
+
   document.getElementById("contactSubmit").addEventListener("click", function (e) {
     const fromEmail = document.getElementById("fromEmail");
-    const emailError = document.getElementById("emailError");
-    const emailSuccess = document.getElementById("emailSuccess");
-  
+    e.preventDefault()
     if (
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
         fromEmail.value
@@ -37,29 +41,37 @@ function showSuccess() {
       try {
         var body = {
           customer: {
-            email: fromEmail,
+            email: fromEmail.value,
             accepts_marketing: true,
             verified_email: true,
           },
         };
-        fetch(siteUrl + "/api/subscribe", {
+        fetch("https://start9.com/api/subscribe", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(body),
         }).then((response) => {
-          if (response.status === 200) {
-            showSuccess();
+          clear()
+          if (response.ok) {
+            showSuccess("Thanks for subscribing!");
           } else {
-            showError(
-              "Something's not working. If you keep getting this error, try us at <a href='https://twitter.com/start9labs' target='_blank'>Twitter</a> instead."
-            );
+            if (response.status = 422) {
+              showError("Already subscribed!")
+            } else {
+              console.error(`Error code: ${response.status} - Details: ${response.statusText}`)
+              showError(
+                "Something's not working. If you keep getting this error, try us at <a href='https://twitter.com/start9labs' rel='noopener noreferrer' target='_blank'>Twitter</a> instead."
+              );
+            }
           }
         });
       } catch (error) {
+        clear()
+        console.error(error)
         showError(
-          "Something's not working. If you keep getting this error, try us at <a href='https://twitter.com/start9labs' target='_blank'>Twitter</a> instead."
+          "Something's not working. If you keep getting this error, try us at <a href='https://twitter.com/start9labs' rel='noopener noreferrer' target='_blank'>Twitter</a> instead."
         );
       }
     } else {
