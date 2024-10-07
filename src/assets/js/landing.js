eeac992a2988ca8998adba5eaa91a773869888b7
Matt Hill <mattnine@protonmail.com>
2024-10-07 08:48:47 -0600

    remove ticker and bitcoin section

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -1,22 +1,3 @@
-import { positionServiceIcons } from '/@root/src/assets/js/bitcoin-animation.js'
-import { initializeSizes, tick, next } from '/@root/src/assets/js/ticker-animation.js'
-
-//////////////////////////////////////////
-// RE-RENDER COMPLEX ANIMATIONS ON WINDOW RESIZE
-
-function onResizeComplete() {
-  positionServiceIcons();
-  initializeSizes();
-  // initialize ticker
-  gsap.delayedCall(next, tick);
-}
-
-let resizeTimeout = setTimeout(onResizeComplete, 400);
-window.onresize = function () {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(onResizeComplete, 400);
-};
-
 //////////////////////////////////////////
 // SHADOW PARALLAX
 
